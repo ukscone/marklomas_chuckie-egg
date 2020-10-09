93bf55ee50a53022c3f5d9256e6b28e5e017297d
ukscone <ukscone@burninghorse.com>
2020-10-09 12:58:32 -0400

    rearrange directories

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+cd src
 cd bbcb
 gcc -Os -I/usr/include/SDL -c sound.c mode2font.c r6502main.c
 ar cr libbbcb.a sound.o r6502main.o mode2font.o
 cd ..
-gcc -Os -o bin/chuckie-egg -I/usr/include/SDL -I../ execute.c library.c main.c -Lbbcb -lbbcb -lSDL -lm
+gcc -Os -o ../chuckie-egg -I/usr/include/SDL -I../ execute.c library.c main.c -Lbbcb -lbbcb -lSDL -lm
